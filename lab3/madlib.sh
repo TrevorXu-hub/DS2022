3a9486d1dd77238e66a13ad9f9db4fa47b096291
XZL <xzl@XZLdeMacBook-Pro.local>
2024-09-19 23:32:17 -0400

    add a lab3

---
+++ lab3/madlib.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+read -p "1. Name an unusual sport: " NOUN1
+read -p "2. Enter an adjective: " ADJE1
+read -p "3. Enter your age: " AGE
+read -p "4. Enter your favorite food: " FOOD
+read -p "5. Name an composer: " COM
+read -p "6. Your birthday: " BIRT
+read -p "7. Do you like Rammstein? : " Y
+read -p "8. Name an weapon: " WEA
+
+echo "Once upon a time, a magic person escape from jail, his name is $NOUN1, but he don't like playing $NOUN1."
+echo "This magic man's grandmom is $AGE years old, pretty ADJE1. He love his grandmom."
+echo "$NOUN1's least favorite food is $FOOD, that's pretty obvious, becuase he is fans of Rammstein!!!"
+echo "The final judgment has come, and no one can stop the great course of history.“
+echo "$BIRT, $NOUN1 went to a bar, at this time, $COM came over and said, "Do you like classical music?" and the wonder man said“$Y !!!"
+echo “No man is an island,"
+echo "entire of itself;"
+echo "every man is a piece of the continent,"
+echo "a part of the main."
+echo "If a clod be washed away by the sea,"
+echo "Europe is the less,"
+echo "as well as if a promontory were,"
+echo "as well as if a manor of thy friend's or of thine own were:"
+echo "any man's death diminishes me,"
+echo "because I am involved in mankind,"
+echo "and,"
+echo "therefore,"
+echo "never send to know for whom the bell tolls;"
+echo "it tolls for thee."
+echo "Magic man picked up a $WEA and walked to his ultimate destiny without hesitation."
